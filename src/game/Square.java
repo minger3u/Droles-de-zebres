337c0e0011dd4c9fb86571d38730a05643e3ffde
Adrien <adrien.claudel.info@gmail.com>
2018-04-05 21:30:06 +0200

    Adding pawn on board

--- src/game/Square.java
+++ src/game/Square.java
@@ -14,8 +14,23 @@ public class Square {
 	private int sector;
 	private Pawn pawn;
 
-	public void adjPown(Pawn p) {
-		this.pawn = p;
+	public Square(int sectore) {
+		this.sector = sector;
+	}
+
+	/**
+	 * Adding pawn on board
+	 * 
+	 * @param p
+	 * @return boolean
+	 */
+	public boolean adjPown(Pawn p) {
+		if(this.pawn == null) {
+			this.pawn = p;
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 }
